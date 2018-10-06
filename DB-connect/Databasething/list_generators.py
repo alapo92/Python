0a0ea85ed867e7f8d2be1f8d076bffa2901dade3
alapo92 <alapo.pete@gmail.com>
2018-10-06 16:06:07 +0200

    some changes

--- DB-connect/Databasething/list_generators.py
+++ DB-connect/Databasething/list_generators.py
@@ -1,26 +1,35 @@
 import random
 
 
-def integers_append(name, limit):
-    for x in range(1, limit):
-        name.append((random.randrange(1, 10000)))
+def generate_list(limit):
+    name = []
+    for x in range(limit):
+        name.append(random.randrange(1, 10000))
     return name
 
 
-def floats_append(name, limit):
-    for x in range(1, limit):
-        name.append((random.random()))
-    return name
+print(generate_list(10))
 
-
-def negatives_append(name, limit):
-    for x in range(1, limit):
-        name.append((random.uniform(-10, 10)))
-    return name
+#
+#
+#
+# def floats_append(limit):
+#     name = []
+#     for x in range(limit):
+#         name.append((random.random()))
+#     return name
+#
+#
+# def negatives_append(limit):
+#     name = []
+#     for x in range(limit):
+#         name.append((random.uniform(-10, 10)))
+#     return name
 
 
 # print(integers_append(integers, 10))
 # print(floats_append(floats, 10))
 # print(negatives_append(negatives, 20))
+
 
 
