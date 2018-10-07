e160e8283e0b9d17e05f7549d86e6c89ecfdc6d3
alapo92 <alapo.pete@gmail.com>
2018-10-07 22:06:46 +0200

    final

--- Web_Development/Flask/Flask Section-4/create_tables.py
+++ Web_Development/Flask/Flask Section-4/create_tables.py
@@ -7,7 +7,7 @@ create_users_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, 
 # gotta use INTEGER instead of int if we wanna auto-increment in SQLite3
 cursor.execute(create_users_table)
 
-create_items_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+create_items_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
 cursor.execute(create_items_table)
 
 connection.commit()
--- Web_Development/Flask/Flask Section-4/models/user.py
+++ Web_Development/Flask/Flask Section-4/models/user.py
@@ -1,41 +1,31 @@
-import sqlite3
+from db import db
 
 
-class UserModel:
-    def __init__(self, _id, username, password):
-        self.id = _id
+class UserModel(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(id=_id).first()
 
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
-
+#
+# This is an API (not a rest api). The API exposes two endpoints
+# those two methods are an interface for the rest of the program to interact with the user
+# like writing or retrieving it from the DB. As long as the API remains unchanged,
+# There is no need to worry about the impact of any further changes to the rest of the program
+#
